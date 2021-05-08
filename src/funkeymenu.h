3e23dd28a86a8dfb6fe7cf52b7d7d618e79155bd
Michel-FK <michel.stempin@funkey-project.com>
2021-05-08 22:58:49 +0200

    rename start_audio_amp to audio_amp
    Signed-off-by: Michel-FK <michel.stempin@funkey-project.com>

--- src/funkeymenu.h
+++ src/funkeymenu.h
@@ -56,8 +56,8 @@ typedef enum {ASPECT_RATIOS} ENUM_ASPECT_RATIOS_TYPES;
 #define SHELL_CMD_CANCEL_SCHED_POWERDOWN    "cancel_sched_powerdown"
 #define SHELL_CMD_SET_LAUNCHER_GMENU2X      "set_launcher gmenu2x"
 #define SHELL_CMD_SET_LAUNCHER_RETROFE      "set_launcher retrofe"
-#define SHELL_CMD_TURN_AMPLI_ON             "start_audio_amp 1"
-#define SHELL_CMD_TURN_AMPLI_OFF            "start_audio_amp 0"
+#define SHELL_CMD_TURN_AMPLI_ON             "audio_amp on"
+#define SHELL_CMD_TURN_AMPLI_OFF            "audio_amp off"
 
 class FunkeyMenu
 {
